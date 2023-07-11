6c7bec196f85a0ba0a0563ae62649488e460af55
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-11 13:18:20 +0530

    Create index.js

---
+++ Middleware/Using Middleware/Problem 1/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+//Authentication middleware
+const authenticateUser = (req, res, next) => {
+  const isAuthenticated = true;
+
+  if (isAuthenticated) {
+    return next();
+  } else {
+    return res.send('Please Login!')
+  }
+}
+
+//Authorization Middleware
+const authorizeAdmin = (req, res, next) => {
+  const isAdmin = false;
+
+  if (isAdmin) {
+    return next();
+  } else {
+    return res.status(403).send('Access Denied');
+  }
+}
+
+//admin
+app.get('/admin', authenticateUser, authorizeAdmin, (req,res) => {
+  res.send('Welcome Admin!!!');
+});
+
+//user
+app.get('/user', authenticateUser, (req, res) => {
+  res.send('Welcome User!!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
