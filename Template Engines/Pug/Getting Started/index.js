1c618d8d691d9cd50c405d898e2220dfef989d8a
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-12 11:57:30 +0530

    Create index.js

---
+++ Template Engines/Pug/Getting Started/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
