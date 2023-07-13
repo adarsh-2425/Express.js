76c57bebce6fcb00660923dad97b2a3163923944
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-13 16:55:17 +0530

    Create simple_code.js

---
+++ Axios/Getting Started/simple_code.js
@@ -0,0 +1,18 @@
+//import axios
+const axios = require('axios');
+
+//jsonplaceholder api
+const API_URI = 'https://jsonplaceholder.typicode.com/posts';
+
+//getPost from api
+const getPosts = async (req, res) => {
+  try {
+    const response = await axios.get(API_URI);
+    const posts = response.data;
+    posts.forEach(post => console.log(post.title));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getPosts();
